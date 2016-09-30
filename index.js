cf13baa0cceca878cbd0edb209f7795367af53a6
Vinayak <vinayakkankanwadi@hotmail.com>
2016-10-01 04:16:32 +1000

    updated index and package to use startstopinterval version 3

---
+++ index.js
@@ -0,0 +1,20 @@
+var ssi= require("startstopinterval");
+
+// sleep time expects milliseconds
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function fn() {
+ var d = new Date();
+ console.log(d);
+}
+
+console.log("start")
+ssi.startStopInterval(true,fn,1000);
+
+sleep(5000).then(() => {
+ console.log("stop");
+ ssi.startStopInterval(false);
+});
+
